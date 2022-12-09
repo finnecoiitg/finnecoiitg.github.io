fca51c753e41b07528e0b49d0b1c6e05b3b72c9e
infinity1729 <anant.bansal2003@gmail.com>
2022-12-09 13:28:42 +0530

    resources till day 5

--- courses/Options/script.js
+++ courses/Options/script.js
@@ -1,6 +1,6 @@
 
 var welcome = document.getElementById('welcome');
-var logo = document.getElementById('logo');
+// var logo = document.getElementById('logo');
 var welcomeTitle = document.getElementById('welcomeTitle');
 var registerBtn = document.getElementById('register-btn');
 var assignment1 = document.getElementById('ass1');
@@ -11,20 +11,21 @@ var paraText = document.getElementById('paraText');
 var title2 = document.getElementById('paraTitle2')
 if (window.innerWidth < 1000) {
     welcome.style.flexDirection = 'column';
-    logo.style.marginTop = '5rem';
-    logo.style.marginBottom = '5rem';
-    logo.style.width = '15vw';
-    welcome.style.height = '100%';
+    // logo.style.marginTop = '5rem';
+    // logo.style.marginBottom = '5rem';
+    // logo.style.width = '15vw';
+    // welcome.style.height = '100%';
     welcome.style.justifyContent = 'center';
     welcomeTitle.style.fontSize = '1.5rem';
     registerBtn.style.padding = "45px 75px";
     registerBtn.style.fontSize = '2rem';
     assignment1.style.flexDirection = 'column';
     assignment1.style.alignItems = 'flex-start';
-    assignment2.style.flexDirection = 'column';
-    assignment2.style.alignItems = 'flex-start';
-    assignment3.style.flexDirection = 'column';
-    assignment3.style.alignItems = 'flex-start';
+    assignment1.style.marginLeft = '1rem';
+    // assignment2.style.flexDirection = 'column';
+    // assignment2.style.alignItems = 'flex-start';
+    // assignment3.style.flexDirection = 'column';
+    // assignment3.style.alignItems = 'flex-start';
     title.style.fontSize = '4rem';
     paraText.style.fontSize = '1.8rem';
     title2.style.fontSize = '4rem';
