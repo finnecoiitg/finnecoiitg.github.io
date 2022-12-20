d7742b20524d2e3d6b67799b35326c7768d9def3
infinity1729 <anant.bansal2003@gmail.com>
2022-12-20 13:12:57 +0530

    updates assignment 2

--- courses/Options/script.js
+++ courses/Options/script.js
@@ -2,6 +2,9 @@
 var welcome = document.getElementById('welcome');
 // var logo = document.getElementById('logo');
 var welcomeTitle = document.getElementById('welcomeTitle');
+var weekContainer1 = document.getElementsByClassName('week-container1')[0];
+var weekContainer2 = document.getElementsByClassName('week-container2')[0];
+var weekContainer3 = document.getElementsByClassName('week-container3')[0];
 var registerBtn = document.getElementById('register-btn');
 var assignment1 = document.getElementById('ass1');
 var assignment2 = document.getElementById('ass2');
@@ -15,6 +18,7 @@ if (window.innerWidth < 1000) {
     // logo.style.marginBottom = '5rem';
     // logo.style.width = '15vw';
     // welcome.style.height = '100%';
+
     welcome.style.justifyContent = 'center';
     welcomeTitle.style.fontSize = '1.5rem';
     registerBtn.style.padding = "45px 75px";
@@ -28,6 +32,9 @@ if (window.innerWidth < 1000) {
     assignment3.style.alignItems = 'flex-start';
     assignment2.style.marginLeft = '1rem';
     assignment3.style.marginLeft = '1rem';
+    // weekContainer3.style.maxWidth="100%";
+    weekContainer2.style.maxWidth="100%";
+    weekContainer1.style.maxWidth="100%";
     title.style.fontSize = '4rem';
     paraText.style.fontSize = '1.8rem';
     title2.style.fontSize = '4rem';
